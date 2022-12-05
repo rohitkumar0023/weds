c4250bb28adb4b477ad200e7b7a132bbd172011d
Poojal12 <104981168+rakshitsaini4@users.noreply.github.com>
2022-12-05 13:28:25 +0530

    weds

---
+++ weds/includes/footer.php
@@ -0,0 +1,8 @@
+    
+    <!-- copyright -->
+    <div class="agileits-w3layouts">
+        <div class="container">
+            <p>© 2022 rohit | </p>
+        </div>
+    </div>
+    <!-- //copyright -->
